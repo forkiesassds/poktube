2fb9550c58c62559db998bb242325413c15c5625
PF94 <45898787+flickPF94@users.noreply.github.com>
2021-01-23 01:18:00 -0500

    Update videos.php

--- videos.php
+++ videos.php
@@ -1,3 +1,17 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>YouTube - Your Digital Video Repository</title>
+<link rel="icon" href="/web/20050701000942im_/http://www.youtube.com/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="/web/20050701000942im_/http://www.youtube.com/favicon.ico" type="image/x-icon">
+<link href="styles_alt.css" rel="stylesheet" type="text/css">
+<link rel="alternate" type="application/rss+xml" title="YouTube " "="" recently="" added="" videos="" [rss]"="" href="https://web.archive.org/web/20050701000942/http://www.youtube.com/rss/global/recently_added.rss">
+</head>
+<?php
+include "header2.php";
+error_reporting(0); //fixing the query issue breaks comment sections.
+?>
+<h1>All videos</h1>
 <?php
 $con=mysqli_connect("localhost", "root", "", "video");
 // Check connection
@@ -8,7 +22,7 @@ echo "Failed to connect to MySQL: " . mysqli_connect_error();
 
 $result = mysqli_query($con,"SELECT * FROM videos");
 
-echo "<table border='1'>
+echo "<table border='2'>
 <tr>
 <th>ID</th>
 <th>Video</th>
@@ -16,6 +30,7 @@ echo "<table border='1'>
 <th>Description</th>
 <th>Uploader</th>
 <th>Uploaded</th>
+<th>Link</th>
 </tr>";
 
 while($row = mysqli_fetch_array($result))
@@ -27,6 +42,7 @@ echo "<td>" . $row['2'] . "</td>";
 echo "<td>" . $row['3'] . "</td>";
 echo "<td>" . $row['4'] . "</td>";
 echo "<td>" . $row['6'] . "</td>";
+echo "<td><a href=\"watch.php?v=" . $row['0'] . "\"</a>Watch</td>";
 echo "</tr>";
 }
 echo "</table>";
