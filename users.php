fe8d1d2d42b4d3c0b9bdafc89a32b69f37ce425b
forkiesassds <icanttellyou@memeware.net>
2021-02-02 21:48:01 +0200

    make users.php look better.

--- users.php
+++ users.php
@@ -6,7 +6,12 @@
 include "header.php";
 error_reporting(0); //fixing the query issue breaks comment sections.
 ?>
-<div class="page_title">All users</div>
+<table width="800" cellspacing="0" cellpadding="0" border="0" align="center">
+	<tbody><tr>
+		<td style="padding-bottom: 25px;" bgcolor="#FFFFFF">
+		<div style="padding: 0px 5px 0px 5px;">
+
+<div class="tableSubTitle">All users</div>
 <?php
 $con=mysqli_connect("localhost", "root", "", "poktube");
 // Check connection
@@ -17,7 +22,7 @@ echo "Failed to connect to MySQL: " . mysqli_connect_error();
 
 $result = mysqli_query($con,"SELECT * FROM users");
 
-echo "<table border='2'>
+echo "<table cellspacing='0' cellpadding='0' border='0' class='roundedTable' bgcolor='#cccccc' border='2'>
 <tr>
 <th>ID</th>
 <th>PFP</th>
@@ -28,13 +33,19 @@ echo "<table border='2'>
 while($row = mysqli_fetch_array($result))
 {
 echo "<tr>";
-echo "<td>" . $row['0'] . "</td>";
-echo "<td><img src=\"content/profpic/" . $row['1'] . ".png" . "\" width=\"64\" height=\"64\"</img></td>";
-echo "<td>" . $row['1'] . "</td>";
-echo "<td>" . $row['3'] . "</td>";
+echo "<td class='moduleEntry_alt'>" . $row['0'] . "</td>";
+echo "<td class='moduleEntry_alt'><img src=\"content/profpic/" . $row['1'] . ".png" . "\" width=\"64\" height=\"64\"</img></td>";
+echo "<td class='moduleEntry_alt'>" . $row['1'] . "</td>";
+echo "<td class='moduleEntry_alt'>" . $row['3'] . "</td>";
 echo "</tr>";
 }
 echo "</table>";
 
 mysqli_close($con);
 ?>
+</div>
+</td>
+</tr>
+</tbody>
+</table>
+</html>
