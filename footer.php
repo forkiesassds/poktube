1f7e434548e0a6460ddf79983c58e2b29085ebef
PF94 <45898787+flickPF94@users.noreply.github.com>
2021-02-06 15:27:50 -0500

    add chaziz logo

--- footer.php
+++ footer.php
@@ -4,6 +4,8 @@
 			<span class="footer"><a href="#">What's New</a> | <a href="#">About Us</a> | <a href="help.php">Help</a> | <a href="#">Developers</a> | <a href="terms.php">Terms of Use</a> | <a href="#">Privacy Policy</a> 
 			<br>
 			<br>
-			Copyright &copy; 2021 Chaziz</td>
+			<a href="https://www.youtube.com/channel/UCMnG3eA5QcSgIPsavuW4ubA"><img src="img/chaziz.png"><br></a>
+			Copyright &copy; 2021 Chaziz
+			</td>
 	</tr>
 </table>
