5aae860ce2212c9153d451ac652efb116d1c0672
forkiesassds <icanttellyou@memeware.net>
2021-02-06 10:04:29 +0200

    make it less retarded?????

--- results.php
+++ results.php
@@ -11,34 +11,44 @@ if( isset($_GET['search']) ){
     $sql = "SELECT * FROM `videodb` WHERE VideoName LIKE '%{$name}%'";
 }
 $result = $connect->query($sql);
+echo "<html>
+<head>
+<title>Search results for ".$_GET['search']."</title>";
 ?>
 <!DOCTYPE html>
-<html>
-<head>
-<title>Basic Search form using mysqli</title>
-<link rel="stylesheet" type="text/css"
-href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 <body>
+<table width="800" cellspacing="0" cellpadding="0" border="0" align="center">
+	<tbody><tr>
+		<td style="padding-bottom: 25px;" bgcolor="#FFFFFF">
+		<div style="padding: 0px 5px 0px 5px;">
 <p>IMPORTANT NOTE: This is retarded</p>
-<div class="container">
-<h2>List of students</h2>
-<table class="table table-striped table-responsive">
+<?php echo "<div class=\"tableSubTitle\">Search results for ".$_GET['search']."</div>" ?>
+<table cellspacing='0' cellpadding='0' border='0' class='roundedTable' bgcolor='#cccccc' border='2'>
+<tbody>
 <tr>
+<th>Thumbnail</th>
 <th>Video Name</th>
+<th>Description</th>
+<th>Uploader</th>
+<th>Uploaded</th>
 <th>Link</th>
 </tr>
+</tbody>
 <?php
 while($row = $result->fetch_assoc()){
     ?>
     <tr>
-    <td><?php echo $row['VideoName']; ?></td>
-    <td><a href="watch.php?v=<?php echo $row['VideoID']; ?>&player=0"><?php echo $row['VideoID']; ?></td>
+	<td class='moduleEntry_alt'><img src="content/thumbs/<?php echo $row['VideoID']; ?>.png" width="120" height="90"</img></td>
+    <td class='moduleEntry_alt'><?php echo htmlspecialchars($row['VideoName']); ?></td>
+	<td class='moduleEntry_alt'><?php echo htmlspecialchars($row['VideoDesc']); ?></td>
+	<td class='moduleEntry_alt'><?php echo htmlspecialchars($row['Uploader']); ?></td>
+	<td class='moduleEntry_alt'><?php echo htmlspecialchars($row['UploadDate']); ?></td>
+    <td class='moduleEntry_alt'><a href="watch.php?v=<?php echo htmlspecialchars($row['VideoID']); ?>&player=0">Watch</td>
     </tr>
     <?php
 }
 ?>
 </table>
-</div>
 </body>
 </html>
