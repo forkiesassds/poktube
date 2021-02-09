9534bef6402732264c5400a5bb6b58050c594aae
PF94 <45898787+flickPF94@users.noreply.github.com>
2021-02-09 09:17:26 -0500

    add console log message

--- player.php
+++ player.php
@@ -54,6 +54,7 @@ Advertisement
 
 
   <script>
+console.log("2012 player loaded: by Crazy frog. Modified by Chaziz and Icanttellyou");
     yt.setMsg({
       'FLASH_UPGRADE': '<div class=\"yt-alert yt-alert-default yt-alert-error  yt-alert-player\"><div class=\"yt-alert-icon\"><img src=\"\/\/s.ytimg.com\/yt\/img\/pixel-vfl3z5WfW.gif\" class=\"icon master-sprite\" alt=\"Alert icon\"><\/div><div class=\"yt-alert-buttons\"><\/div><div class=\"yt-alert-content\" role=\"alert\">    <span class=\"yt-alert-vertical-trick\"><\/span>\n    <div class=\"yt-alert-message\">\n            You need to upgrade your Adobe Flash Player to watch this video. <br> <a href=\"http:\/\/get.adobe.com\/flashplayer\/\">Download it from Adobe.<\/a>\n    <\/div>\n<\/div><\/div>'
     });
