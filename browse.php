4895d97b248c5a064774350e696e477c7c5ed8be
forkiesassds <icanttellyou@memeware.net>
2021-02-24 21:32:22 +0200

    rework the video list pages
    hoorayy! i fixed most of the problems

--- browse.php
+++ browse.php
@@ -2,19 +2,16 @@
 include("header.php"); 
 if(!isset($_GET["page"])){
 	$page = 1;
-}
-if(isset($_GET["page"])){
-if($_GET["page"] == 1) {
-	$page = 1;
-}
-if($_GET["page"] && $_GET["page"] > 1){
-	$page = $_GET["page"] * 20;
-}
+} else
+{
+	$page = $_GET["page"];
 }
 $page = $page - 1;
 $sql = mysqli_query($connect, "SELECT * FROM videodb"); //instructions for sql
+
 $count = 0;
 $pages = 0;
+$row = 0;
 
 while ($fetch = mysqli_fetch_assoc($sql)) { //go forward with instructions
 $count++;
@@ -25,38 +22,17 @@ if($count == 20) {
 }
 ?>
 <title>Browse - PokTube</title>
-<table width="790" align="center" cellpadding="0" cellspacing="0" border="0" bgcolor="#CCCCCC">
-	<tbody><tr>
-		<td><img src="img/box_login_tl.gif" width="5" height="5"></td>
-		<td><img src="img/pixel.gif" width="1" height="5"></td>
-		<td><img src="img/box_login_tr.gif" width="5" height="5"></td>
-	</tr>
-	<tr>
-		<td><img src="img/pixel.gif" width="5" height="1"></td>
-		<td width="780">
-			<div class="moduleTitleBar">
-							<table cellpadding="0" cellspacing="0" border="0">
-				<tbody><tr valign="top">
-					<td width="260">
-						<div class="moduleTitle">Most Recent</div>
-					</td>
-					<td width="260" align="center">
-						<div style="font-weight: normal; font-size: 11px; color: #444444">
-							
-						</div>
-					</td>
-					<td width="260" align="right">
-					</td>
-				</tr>
-			</tbody></table>
+<div class="ui container">
+	<h4 class="ui top attached inverted header">
+		<div id="homepage-featured-more-top">
+			<span>Most Recent</span>
+		</div>
+	</h4>
+		<div class="ui bottom attached segment">
+			<div class="ui center aligned grid">
+<?php
+$vidlist = mysqli_query($connect, "SELECT * FROM videodb ORDER by `UploadDate` DESC");
 
-			</div>
-				
-			<div class="moduleFeatured"> 
-								<table width="770" cellpadding="0" cellspacing="0" border="0">
-
-				<tbody><?php
-$vidlist = mysqli_query($connect, "SELECT * FROM videodb ORDER by `UploadDate` DESC LIMIT ".$page.", 20");
 $count = 0;
 
 while ($fetch = mysqli_fetch_assoc($vidlist)) {
@@ -65,31 +41,32 @@ $namevideolist = htmlspecialchars($fetch['VideoName']);
 $uploadervideolist = htmlspecialchars($fetch['Uploader']);
 $uploadvideolist = $fetch['UploadDate'];
 $viewsvideolist = htmlspecialchars($fetch['ViewCount']);
-
-	if($count == 0) {
-		echo "<tr valign='top'>";
-	}
-	echo "<td width='20%' align='center'>
-			<a href='watch.php?v=".$idvideolist."'><img src='./content/thumbs/".$idvideolist.".png' onerror=\"this.src='img/default.png'\" width='120' height='90' class='moduleFeaturedThumb'></a>
+	if ($count > 20*$page) {
+	echo "<div class=\"four wide column\">
+			<a href='watch.php?v=".$idvideolist."'><img src='./content/thumbs/".$idvideolist.".png' onerror=\"this.src='img/default.png'\" width='160' height='120' class='moduleFeaturedThumb'></a>
 			<div class='moduleFeaturedTitle'><a href='watch.php?v=".$idvideolist."'>".$namevideolist."</a></div>
 			<div class='moduleFeaturedDetails'>
 				Added: ".$uploadvideolist."<br>
 				by <a href='profile.php?user=".$uploadervideolist."'>".$uploadervideolist."</a>
 			</div>
-		</td>";
+		</div>";
+	}
 	$count++;
-	if($count == 4) {
+	if ($count == 20*$page+5) {
+		if ($row == 4) {
+			break;
+		}
 		echo "</tr>";
-		$count = 0;
+		$count = 20 * $page;
+		$row++;
 	}
-	}
+}
 ?>
-				</tbody></table>
-
 			</div>
+		</div>
 
 			<!-- begin paging -->
-			<div style="font-size: 13px; font-weight: bold; color: #444; text-align: right; padding: 5px 0px 5px 0px;">
+			<div class="ui buttons">
 				<?php
 				$pagecount = 0;
 				while($pagecount !== $pages) {
@@ -97,18 +74,13 @@ $viewsvideolist = htmlspecialchars($fetch['ViewCount']);
 						echo "Browse Pages:";
 					}
 					$pagecount++;
-					echo "<span style='background-color: #CCC; padding: 1px 4px 1px 4px; border: 1px solid #999; margin-right: 5px;'><a href='browse.php?page=".$pagecount."'>".$pagecount."</a></span>";
+
+					echo "<a class=\"ui button\" href='browse.php?page=".$pagecount."'>".$pagecount."</a>";
+					
 				}
 				?>
 			</div>
+		</div>
+	</div>
 			<!-- end paging -->
-		</td>
-		<td><img src="img/pixel.gif" width="5" height="1"></td>
-	</tr>
-	<tr>
-		<td><img src="img/box_login_bl.gif" width="5" height="5"></td>
-		<td><img src="img/pixel.gif" width="1" height="5"></td>
-		<td><img src="img/box_login_br.gif" width="5" height="5"></td>
-	</tr>
-</tbody></table>
 <?php include("footer.php"); ?>
