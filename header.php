59128199627c1e6001374449192376847592ced2
PF94 <45898787+flickPF94@users.noreply.github.com>
2021-02-25 15:06:13 -0500

    oh my fucking god WHYYYYYYYYYY

--- header.php
+++ header.php
@@ -17,7 +17,11 @@ if ($detail2["registeredon"] == null) {
 <head>
 <script src="assets/jquery-3.5.1.min.js"></script>
 <script src="semantic/dist/semantic.min.js"></script>
+<link rel="stylesheet" type="text/css" href="semantic/dist/components/menu.css">
+<link rel="stylesheet" type="text/css" href="semantic/dist/components/image.css">
+<link rel="stylesheet" type="text/css" href="semantic/dist/components/transition.css">
 <link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
+<link rel="stylesheet" type="text/css" href="img/chaziz_edit.css">
 <link rel="icon" href="favicon.ico" type="image/x-icon">
 <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 <meta name="theme-color" content="#f28900">
