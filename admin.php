1a78f353d0ed62456f94dc6b4a4829d0078f5608
forkiesassds <icanttellyou@memeware>
2021-02-28 20:38:23 +0200

    add php version number to admin panel

--- admin.php
+++ admin.php
@@ -7,6 +7,8 @@ if(!$admin=1) {
 <div class="ui container">
 	<h1>Welcome, <?php echo $_SESSION["username"];?></h1>
 <pre>
+<strong>PHP Version:</strong>
+<?php echo phpversion()."\n";?>
 <strong>Uptime:</strong>
 <?php system("uptime"); ?>
 <strong>System Information:</strong>
