9daacb4b279f49d60e12835c0b8ffcf875ac7031
PF94 <45898787+flickPF94@users.noreply.github.com>
2021-03-15 14:30:49 -0400

    featured videos and shitty header redesign that's unfinished

--- index.php
+++ index.php
@@ -36,46 +36,39 @@ if(($_GET["vexist"]) == 0){
 }
 } else {
 	$vexist = null;
-}
-?>
-<div id="homepage-main-content">
-	<tr valign="top">
-		<td style="padding-right: 15px;">
-					<table class="roundedTable" width="650" align="center" cellpadding="0" cellspacing="0" border="0" bgcolor="#e5ecf9">
-			<tr>
-				<td><img src="img/box_login_tl.gif" width="5" height="5"></td>
-				<td width="100%"><img src="img/pixel.gif" width="1" height="5"></td>
-				<td><img src="img/box_login_tr.gif" width="5" height="5"></td>
-			</tr>
-			<tr>
-				<td><img src="img/pixel.gif" width="5" height="1"></td>
-				<td width="585">
-									<table width="100%" cellpadding="0" cellspacing="0" border="0">
-					<tr valign="top">
-					<td width="33%" style="border-right: 1px dashed #369; padding: 0px 10px 10px 10px; color: #444;">
-					<div style="font-size: 16px; font-weight: bold; margin-bottom: 5px;"><img src="img/silk/television.png"> <a href="browse.php">Watch</a></div>
-					Instantly find and watch streaming videos.
-					</td>
-					<td width="33%" style="border-right: 1px dashed #369; padding: 0px 10px 10px 10px; color: #444;">
-					<div style="font-size: 16px; font-weight: bold; margin-bottom: 5px;"><img src="img/silk/add.png"> <a href="my_videos_upload.php">Upload</a></div>
-					Quickly upload and tag videos in almost any video format.
-					</td>
-					<td width="33%" style="padding: 0px 10px 10px 10px; color: #444;">
-					<div style="font-size: 16px; font-weight: bold; margin-bottom: 5px;"><img src="img/silk/link.png"> <a href="my_friends_invite.php">Share</a></div>
-					Easily share your videos with your family, friends, or co-workers.
-					</td>
-					</tr>
-				</table>
-				
-				</td>
-				<td><img src="img/pixel.gif" width="5" height="1"></td>
-			</tr>
-			<tr>
-				<td><img src="img/box_login_bl.gif" width="5" height="5"></td>
-				<td><img src="img/pixel.gif" width="1" height="5"></td>
-				<td><img src="img/box_login_br.gif" width="5" height="5"></td>
-			</tr>
-		</table>
+}?>
+
+<div id='homepage-main-content'>
+<div class='homepage-content-block sponsored-videos-block'>
+		<div class='homepage-block-heading homepage-block-heading-gray'>Promoted Videos</div>
+		<div>
+
+<?php
+
+$sqlSponsered = mysqli_query($connect, "SELECT * FROM videodb WHERE `isApproved` = '1' ORDER BY RAND() DESC LIMIT 4"); //instructions for sql, also WHERE with ORDER BY works, icty, you said that it didn't in FEB 24 2021, you're wrong.
+while ($fetch = mysqli_fetch_assoc($sqlSponsered)) {
+$idvideolistSponsered = $fetch['VideoID'];
+$namevideolistSponsered = htmlspecialchars($fetch['VideoName']);
+$uploadervideolistSponsered = htmlspecialchars($fetch['Uploader']); // get recommendations information
+$uploadvideolistSponsered = htmlspecialchars($fetch['UploadDate']); // get recommendations information
+$descvideolistSponsered = htmlspecialchars($fetch['VideoDesc']);
+$viewsvideolistSponsered = htmlspecialchars($fetch['ViewCount']);
+echo "<div class='homepage-sponsored-video'>
+				<div class='videoIconWrapperOuter'>
+					<div class='videoIconWrapperInner'>
+					<div class='vstill'><a href='watch.php?v=$idvideolistSponsered'><img src='content/thumbs/".$idvideolistSponsered.".png' onerror=\"this.src='img/default.png'\"  class='vimg120'></a></div>
+					</div>
+				</div>
+				<div class='vtitle smallText'>
+					<a href='watch.php?v=$idvideolistSponsered'>$namevideolistSponsered</a>
+				</div>
+				<div class='vfacets'>
+				<a href='/web/20090101130443/http://youtube.com/user/VenetianPrincess' class='dg'>$uploadervideolistSponsered</a>
+				</div>
+			</div>";};
+		?>
+				<div class='spacer-sm'></div>
+		</div>
 <!-- begin recently featured -->
 <div id="homepage-featured-heading">
 			<div id="homepage-featured-more-top"><a id="hpVideoListMoreLink" href="browse.php" onmousedown="urchinTracker('/Events/Home/SeeMore/Featured/Top');">See More Featured Videos</a></div>
@@ -88,10 +81,16 @@ if(($_GET["vexist"]) == 0){
 	<div id='hpFeatured'>
 		<div class='video-entry'>
 <?php
-$sql = mysqli_query($connect, "SELECT * FROM videodb WHERE `isApproved` = '1' ORDER BY RAND() DESC LIMIT 10"); //instructions for sql
+$sql = mysqli_query($connect, "SELECT * FROM videodb WHERE `isApproved` = '1' ORDER BY RAND() DESC LIMIT 10"); //instructions for sql, also WHERE with ORDER BY works, icty, you said that it didn't in FEB 24 2021, you're wrong.
 
 while ($fetch = mysqli_fetch_assoc($sql)) { //go forward with instructions
 $idvideolist = $fetch['VideoID'];
+$lengthlist = 0;
+if($fetch['VideoLength'] > 3600) {
+	$lengthlist = floor($fetch['VideoLength'] / 3600) . ":" . gmdate("i:s", $fetch['VideoLength'] % 3600);
+} else { 
+	$lengthlist = gmdate("i:s", $fetch['VideoLength'] % 3600) ;
+};
 $namevideolist = htmlspecialchars($fetch['VideoName']);
 $uploadervideolist = htmlspecialchars($fetch['Uploader']); // get recommendations information
 $uploadvideolist = htmlspecialchars($fetch['UploadDate']); // get recommendations information
@@ -101,8 +100,9 @@ echo "<div class='video-entry'>
    <div class='v120WideEntry'>
       <div class='v120WrapperOuter'>
          <div class='v120WrapperInner'>
-            <a id='video-url-muP9eH2p2PI' href='watch.php?v=$idvideolist&player=0' rel='nofollow'><img title='$namevideolist' src='content/thumbs/".$idvideolist.".png' onerror=\"this.src='img/default.png'\" class='vimg120' qlicon='muP9eH2p2PI' alt='$namevideolist'></a>
-         </div>
+            <a id='video-url-muP9eH2p2PI' href='watch.php?v=$idvideolist' rel='nofollow'><img title='$namevideolist' src='content/thumbs/".$idvideolist.".png' onerror=\"this.src='img/default.png'\" class='vimg120' qlicon='muP9eH2p2PI' alt='$namevideolist'></a>
+         <div class='video-time'><span id='video-run-time-muP9eH2p2PI'>$lengthlist</span></div>
+		 </div>
       </div>
    </div>
    <div class='video-main-content' id='video-main-content-muP9eH2p2PI'>
@@ -120,11 +120,11 @@ echo "<div class='video-entry'>
    </div>
    <div class='video-clear-list-left'></div>
 </div>";
-}
+};
 //<a href='watch.php?v=$idvideolist&player=1'>Flash Player</a> - <a href='watch.php?v=$idvideolist&player=2'>ActiveX</a>
 ?>
 			<!-- end recently featured -->
-</div></div></div></div></div>
+</div></div></div></div></div></div>
 
 <div id="homepage-side-content">
    <div class="homepage-side-block" id="homepage-whats-new-block">
@@ -132,11 +132,11 @@ echo "<div class='video-entry'>
          <div class="homepage-block-heading" style="color:#CC6600">What's New</div>
          <div class="clear"></div>
          <div class="bottomBorderDotted"></div>
+		 <b style="color:#CC6600">Reverted redesign</b><br>
+		 Squarebracket's Semantic UI redesign has been reverted.<br><br>
 		 <b style="color:#CC6600">New server</b><br>
 		 PokTube is no longer hosted on a laptop. It has been moved to be hosted on a Debian server with Nginx.<br><br>
 		 Since we haven't figured everything out yet, video uploading and playback is broken for the time being.<br><br>
-		 <b style="color:#CC6600">New player</b><br>
-		 End my suffering, Flash and ActiveX players are no longer available for the time being.<br><br>
          <div style="font-size: 1px; height: 1px;"><br></div>
       </div>
       <img class="homepage-yellow-block-bot" src="https://web.archive.org/web/20090101094601im_/http://s.ytimg.com/yt/img/pixel-vfl73.gif">
