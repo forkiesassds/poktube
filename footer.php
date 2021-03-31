eeefdd89eef7cbcb1a586129ebece2c8b077f8b9
PF94 <45898787+flickPF94@users.noreply.github.com>
2021-03-30 22:46:11 -0400

    improve footer + gradient on button

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 	<tr>
 		<td align="center" valign="center">
 			<span class="footer"> 
-			<a href="#">What's New</a> | <a href="#">About Us</a> | <a href="help.php">Help</a> | <a href="#">Developers</a> | <a href="terms.php">Terms of Use</a> | <a href="#">Privacy Policy</a>
+			<a href="#">What's New</a> | <a href="about.php">About Us</a> | <a href="help.php">Help</a> | <a href="#">Developers</a> | <a href="terms.php">Terms of Use</a> | <a href="#">Privacy Policy</a>
 			<br><br>
 			<a href="https://www.youtube.com/channel/UCMnG3eA5QcSgIPsavuW4ubA"><img src="img/chaziz.png"><br></a>
 			Copyright &copy; 2021 Chaziz <br/>
