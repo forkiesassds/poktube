8d345846548ccacd126e6689ca5069c850be5b22
PF94 <45898787+flickPF94@users.noreply.github.com>
2021-04-14 21:27:35 -0400

    Update index.php

--- index.php
+++ index.php
@@ -332,13 +332,8 @@ echo "<div class='headerRCBox'>
 	</div>
 	<div class="hpContentBlock">
 			<div class="headerRCBox">
-	<b class="rch">
-	<b class="rch1"><b></b></b>
-	<b class="rch2"><b></b></b>
-	<b class="rch3"></b>
-	<b class="rch4"></b>
-	<b class="rch5"></b>
-	</b> <div class="content"><span class="headerTitle">Active Channels</span></div>
+	<div class="rch">
+	</div> <div class="content"><span class="headerTitle">Active Channels</span></div>
 	</div>
 
 		<div class="contentBox">
